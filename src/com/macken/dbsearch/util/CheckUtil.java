eb2eddf89fe9cbbae14f04540bdfd95e53c19b8e
macken <jundongtan@gmail.com>
2013-05-08 23:33:30 +0800

    111

--- src/com/macken/dbsearch/util/CheckUtil.java
+++ src/com/macken/dbsearch/util/CheckUtil.java
@@ -2,7 +2,7 @@ package com.macken.dbsearch.util;
 
 public class CheckUtil {
 	public static boolean checkWords(String content) {
-		String[] keywords = { "青稚","主题歌" };
+		String[] keywords = { "男朋友", "蓝朋友", "男友", "蓝友", "征友", "蒸友" };
 		for (int i = 0; i < keywords.length; i++) {
 			if (content.contains(keywords[i])) {
 				return true;
