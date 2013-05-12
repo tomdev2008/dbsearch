965ebef987884a5085223850611b5f6a0867e6f3
macken <jundongtan@gmail.com>
2013-05-12 09:25:40 +0800

    Signed-off-by: macken <jundongtan@gmail.com>

--- src/com/macken/dbsearch/crawler/UploadThread.java
+++ src/com/macken/dbsearch/crawler/UploadThread.java
@@ -5,12 +5,14 @@ import com.macken.dbsearch.util.FMUtil;
 public class UploadThread extends Thread {
 	@Override
 	public void run() {
-		try {
-			FMUtil.gen();
-			FMUtil.exec("scp -r /search/dbsearch/html/ root@173.231.52.194:/search/");
-			this.sleep(10 * 60 * 1000);
-		} catch (Exception e) {
-			e.printStackTrace();
+		while(true){
+			try {
+				FMUtil.gen();
+				FMUtil.exec("scp -r /search/dbsearch/html/ root@173.231.52.194:/search/");
+				this.sleep(10 * 60 * 1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
