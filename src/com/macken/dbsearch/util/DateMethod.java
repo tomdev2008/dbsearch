e936f022cad52483bcfa2bb37e3a10055dcf76cd
mackentan <jundongtan@gmail.com>
2013-05-13 16:54:23 +0800

    Signed-off-by: mackentan <jundongtan@gmail.com>

--- src/com/macken/dbsearch/util/DateMethod.java
+++ src/com/macken/dbsearch/util/DateMethod.java
@@ -9,7 +9,7 @@ import freemarker.template.TemplateModelException;
 public class DateMethod implements TemplateMethodModel {
 
 	public Object exec(List arg) throws TemplateModelException {
-		Long date = (Long) arg.get(0);
+		Long date = Long.parseLong((String) arg.get(0));
 		Date d = new Date(date);
 		return DateUtil.format(d, "yyyy-MM-dd HH:mm");
 	}
