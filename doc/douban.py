4ea9fa12f4cd24d108cc134bba350f1f1cfb8a40
mackentan <jundongtan@gmail.com>
2013-05-20 20:08:26 +0800

    Signed-off-by: mackentan <jundongtan@gmail.com>

--- doc/douban.py
+++ doc/douban.py
@@ -3,12 +3,13 @@ import sys, time, os, re
 import urllib, urllib2, cookielib
 
 loginurl = 'https://www.douban.com/accounts/login'
-cookie = cookielib.CookieJar()
+#cookie = cookielib.CookieJar()
+cookie = cookielib.MozillaCookieJar("cookiedata");
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 
 params = {
-"form_email":"email",
-"form_password":"password",
+"form_email":"403332023@qq.com",
+"form_password":"1989hao",
 "source":"index_nav"
 }
 
@@ -25,20 +26,21 @@ if response.geturl() == "https://www.douban.com/accounts/login":
 		res=urllib.urlretrieve(url, 'v.jpg')
 		captcha=re.search('<input type="hidden" name="captcha-id" value="(.+?)"/>' ,html)
 		if captcha:
-			vcode=raw_input('auth code')
+			vcode=raw_input('auth code:')
 			params["captcha-solution"] = vcode
 			params["captcha-id"] = captcha.group(1)
 			params["user_login"] = "login"
 			response=opener.open(loginurl, urllib.urlencode(params))
 			''' redirect index '''
 			if response.geturl() == "http://www.douban.com/":
+				cookie.save("cookiedata")
 				print 'login success ! '
 				print 'reday to public'
 				p={"ck":""}
 				c = [c.value for c in list(cookie) if c.name == 'ck']				
 				if len(c) > 0:
 					p["ck"] = c[0].strip('"')		
-
+				
 				addtopicurl="http://www.douban.com/group/bj/new_topic"
 				res=opener.open(addtopicurl)
 				html=res.read()
@@ -47,6 +49,8 @@ if response.geturl() == "https://www.douban.com/accounts/login":
 				p["topic_id"] = m.group(1)
 				m= re.search('<input type="hidden" name="topic_id_sig" value="(.+?)">', html)	
 				p["topic_id_sig"] = m.group(1)'''
+				
+				
 				p["rev_title"] = 'title'
 				p["rev_text"] = 'send body'
 				p["rev_submit"] = 'r'
@@ -55,6 +59,6 @@ if response.geturl() == "https://www.douban.com/accounts/login":
 				request.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11")
 				request.add_header("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
 				request.add_header("Origin", "http://www.douban.com")
-				request.add_header("Referer", "http://www.douban.com/group/python/new_topic")
+				request.add_header("Referer", "http://www.douban.com/group/bj/new_topic")
 				opener.open(request, urllib.urlencode(p))
 	
