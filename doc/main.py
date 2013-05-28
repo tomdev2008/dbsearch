08f06b1f9ca64546b01a5f2ac3ab6ea684454716
macken <jundongtan@gmail.com>
2013-05-28 23:53:15 +0800

    Signed-off-by: macken <jundongtan@gmail.com>

--- doc/main.py
+++ doc/main.py
@@ -42,6 +42,16 @@ def searchaddgroup(account):
 		url='http://www.douban.com/group/explore?start='+str(start)+'&tag=%e6%98%9f%e5%ba%a7'
 		account.addmoregroup(url)
 		start+=20
+def sitesearchgroup(account):
+	start=0
+	while start <=120:
+		url='http://www.douban.com/group/search?start='+str(start)+'&cat=1019&q=%E5%8C%97%E4%BA%AC'
+		try:
+			account.addsearchgroup(url)
+		except:
+			print 'error'
+		start+=20
+		
 def publictopic():
 	while True:
 		for a in aList:
@@ -50,7 +60,7 @@ def publictopic():
 				a.addtopic(t,c)
 		time.sleep(60*10)
 	
-accountsdict={'jokerdu@126.com':"73054480:Xu/RthOBGLc"}
+accountsdict={"andrewsunli@163.com":"73054568:l+X0Fn8qBSE"}
 #'1505014246@qq.com':'72538099:9ujSxqAJjBQ',
 
 aList=[]
@@ -62,14 +72,14 @@ contents={'default':'123.txt',
 		'c1':'content/c1.txt',
 		'c2':'content/c2.txt'}
 #aList[0].removeallgroup()
-#searchaddgroup(aList[1])
 #searchaddgroup(aList[0])
-
+sitesearchgroup(aList[0])
 #for a in aList:
 #	searchaddgroup(a)
 
-publictopic()
+#publictopic()
 #print randtopic()
+
 
 
 
