d75ad0fa0d1c674874ba16721935f1b15e1d5095
mackentan <jundongtan@gmail.com>
2013-06-18 13:01:58 +0800

    Signed-off-by: mackentan <jundongtan@gmail.com>

--- src/com/macken/dbsearch/crawler/UploadSchedule.java
+++ src/com/macken/dbsearch/crawler/UploadSchedule.java
@@ -1,5 +1,6 @@
 package com.macken.dbsearch.crawler;
 
+import com.macken.dbsearch.OutputJson;
 import com.macken.dbsearch.util.FMUtil;
 
 public class UploadSchedule {
@@ -10,6 +11,7 @@ public class UploadSchedule {
 	public static void main(String[] args) {
 		try {
 			FMUtil.gen();
+			OutputJson.genJson();
 			FMUtil.exec("scp -r /search/dbsearch/html/ root@173.231.52.194:/search/");
 		} catch (Exception e) {
 			e.printStackTrace();
